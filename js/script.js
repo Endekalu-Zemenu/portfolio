61ceab948515119b06a6b2bc8f4023fa07245002
Endekalu-Zemenu <155809658+Endekalu-Zemenu@users.noreply.github.com>
2024-01-06 11:54:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* =========================== typing animation ====================== */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "web designer",
+    "web developer",
+    "Javascript Enthusias",
+    "student",
+  ],
+  typeSpeed: 70,
+  backSpeed: 60,
+  loop: true,
+});
